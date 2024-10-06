b7613978db0934656d08fd926c3c229df35bb11d
arturwhite1997 <whiteartur1997@gmail.com>
2024-10-06 17:48:23 +0300

    add hero page

---
+++ select-hero.js
@@ -0,0 +1,69 @@
+const heroBody = document.querySelector(".hero-body");
+const selectButtons = document.querySelectorAll('.select-btn');
+
+const heroes = [
+    {
+        name: "Terrestrial",
+        image: "https://eyes.nasa.gov/apps/exo/assets/image/thumbnail/exoplanet/terrestrial-list.webp"
+    },
+    {
+        name: "Super-Earth",
+        image: "https://eyes.nasa.gov/apps/exo/assets/image/thumbnail/exoplanet/gj-15-a-b-list.webp"
+    },
+    {
+        name: "Gas-Giant",
+        image: "https://eyes.nasa.gov/apps/exo/assets/image/thumbnail/exoplanet/kepler-16-b-list.webp"
+    },
+    {
+        name: "Neptune-Like",
+        image: "https://eyes.nasa.gov/apps/exo/assets/image/thumbnail/exoplanet/au-mic-b-list.webp"
+    }
+];
+function setHeroToLS(heroName) {
+    localStorage.setItem("hero", JSON.stringify(heroes.find(hero => hero.name === heroName)))
+}
+
+function handleHeroSelect() {
+    console.log(this)
+    const planetType = this.dataset.id;
+    switch (planetType) {
+        case "Terrestrial": {
+            setHeroToLS("Terrestrial")
+            break;
+        }
+        case "Super-Earth": {
+            setHeroToLS("Super-Earth")
+            break;
+        }
+        case "Gas-Giant": {
+            setHeroToLS("Gas-Giant")
+            break;
+        }
+        case "Neptune-Like": {
+            setHeroToLS("Neptune-Like")
+            break;
+        }
+    }
+    window.location.assign('hero.html');
+}
+
+selectButtons.forEach(button => {
+    button.addEventListener('click', handleHeroSelect);
+});
+
+const hero = JSON.parse(localStorage.getItem("hero"))
+
+console.log("hero", hero)
+if(hero) {
+    const header = `
+        <div class="header">
+            <div class="planet-card" style="display: flex; align-items: center; column-gap: 10px;">
+                <h2 id="hero-name">You are ${hero.name}</h2>
+                <img id="hero-image" class="planet-image-small" src="${hero.image}"/>
+            </div>
+        </div>
+    `;
+
+    heroBody.insertAdjacentHTML('beforeend', header);
+}
+
